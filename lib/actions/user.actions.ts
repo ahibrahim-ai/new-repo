1d636c19ffc4065099c2e04a6ca9afca4ed5fe5b
Sous Abraham <theaateam24@gmail.com>
2024-06-12 08:50:17 +0100

    new hero

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,11 +2,11 @@
 
 import { revalidatePath } from 'next/cache'
 
-import { connectToDatabase } from '@/lib/database'
+import { connectToDatabase } from '../database'
 import User from '../database/models/user.model'
 import Order from '../database/models/order.model'
 import Event from '../database/models/event.model'
-import { handleError } from '@/lib/utils'
+import { handleError } from '../utils'
 
 import { CreateUserParams, UpdateUserParams } from '../../app/types'
 
